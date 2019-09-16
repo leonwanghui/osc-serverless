18e77482f2613df1dd89e349935c02a15ea4da3f
leonwanghui <wanghui71leon@gmail.com>
2019-09-16 11:40:14 +0800

    Move web module to webpack-dev-server
    Signed-off-by: leonwanghui <wanghui71leon@gmail.com>

--- osc-runtime-js/src/osc_runtime_bundler.js
+++ osc-runtime-js/src/osc_runtime_bundler.js
@@ -1,92 +1,123 @@
 const rust =
   import ('../bundler');
 import * as YAML from 'yamljs';
-import * as req from './request_bundler';
+import * as request from 'superagent';
 
-let strdata = `
-kind: ComputeResource
-metadata:
-  cloud_provider:
-    name: huaweicloud
-  labels:
-    app: etcd
-spec:
-  name: newserver
-  description: This is a new server for testing
-  availability_zone: cn-north-1a # 华北-北京一
-  cloud_provider: # Deprecated, please use metadata
-    name: huaweicloud
-  cloud_server_request_fragment:
-    imageRef: f1dd2272-7041-479e-9663-646632b6ac00 # Ubuntu 16.04 server 64bit
-    root_volume:
-      volumetype: SATA
-      size: 40
-    flavorRef: t6.small.1 # 1vCPUs | 1GB
-    key_name: KeyPair-2e4a
-    user_data: "/etc/osc-config/app_install_etcd.sh"
-    vpcid: e86bd162-8136-4227-a076-825161c95d29 # vpc-default | 192.168.0.0/16
-    nics:
-      - subnet_id: ff5330b5-c6f2-4983-8ba1-3fc81fe816e9 # subnet-default | 192.168.0.1
-        # ip_address: x.x.x.x
-    publicip:
-      # id: 501e1f51-b00c-4d88-a586-29948c43910b
-      eip: # create a new public ip
-        ip_type: 5_sbgp
-        bandwidth:
-          name: newbandwidth
-          sharetype: PER
-          size: 1
-    security_groups:
-      - id: 86bd6416-3d4b-45d3-bf6e-605b9326015a # Sys-default
-    count: 1
-`
-let data = YAML.parse(strdata);
-// Set the operation manually to pass the test case.
-data.metadata.operation = 'create';
+let config = {
+  user: 'username',
+  password: 'password'
+};
 
-rust
-  .then(m => {
-    let body = m.load_resource_to_js(data);
-    console.log(body);
-    switch (body.kind) {
-      case 'ComputeResource':
-        switch (body.operation) {
-          case 'create':
-            req.compute_resource_create(body.params);
-            break;
-          case "delete":
-            req.compute_resource_delete(body.params.name, body.params.cloud_provider, body.params.optional);
-            break;
-          default:
-            console.log('operation {' + body.operation + '} not supported!');
-        };
-        break;
-      case 'StorageResource':
-        switch (body.operation) {
-          case 'create':
-            req.storage_resource_create(body.params);
-            break;
-          case "delete":
-            req.storage_resource_delete(body.params.name, body.params.cloud_provider);
-            break;
-          default:
-            console.log('operation {' + body.operation + '} not supported!');
-        };
-        break;
-      case 'NetworkResource':
-        switch (body.operation) {
-          case 'create':
-            req.network_resource_create(body.params);
-            break;
-          case "delete":
-            req.network_resource_delete(body.params.name, body.params.cloud_provider);
-            break;
-          default:
-            console.log('operation {' + body.operation + '} not supported!');
-        };
-        break;
-      default:
-        console.log('resource {' + body.kind + '} not supported!');
-    }
-  })
-  .catch(console.error);
+/**
+ * @param {string} operate
+ * @param {any} filedata
+ * @returns {any}
+ */
+export function load(operate, filedata) {
+  let data = YAML.parse(filedata);
+  // Set the operation manually to pass the test case.
+  data.metadata.operation = operate;
+
+  let value = rust
+    .then(m => {
+      let body = m.load_resource_to_js(data);
+      switch (body.kind) {
+        case 'ComputeResource':
+          switch (body.operation) {
+            case 'create':
+              request.post('/v1alpha/compute_resources')
+                .auth(config.user, config.password)
+                .send(body.params)
+                .set('Content-Type', 'application/json')
+                .set('Accept', 'application/json')
+                .end(function(err, res) {
+                  if (err) throw err;
+                  console.log(res.body);
+                  return res.body
+                });
+              break
+            case "delete":
+              let opts = body.params.optional;
+              request.delete('/v1alpha/compute_resources/' + body.params.name +
+                  '?cloud_provider=' + body.params.cloud_provider +
+                  '&delete_publicip=' + opts.delete_publicip +
+                  '&delete_volume=' + opts.delete_volume)
+                .auth(config.user, config.password)
+                .set('Accept', 'application/json')
+                .end(function(err, res) {
+                  if (err) throw err;
+                  console.log('delete compute resource {' + body.params.name + '} finished!');
+                  return 'delete compute resource {' + body.params.name + '} finished!'
+                });
+              break
+            default:
+              console.error('operation {' + body.operation + '} not supported!');
+          };
+          break;
+        case 'StorageResource':
+          switch (body.operation) {
+            case 'create':
+              request.post('/v1alpha/storage_resources')
+                .auth(config.user, config.password)
+                .send(body.params)
+                .set('Content-Type', 'application/json')
+                .set('Accept', 'application/json')
+                .end(function(err, res) {
+                  if (err) throw err;
+                  console.log(res.body);
+                  return res.body
+                });
+              break
+            case "delete":
+              request.delete('/v1alpha/storage_resources/' + body.params.name +
+                  '?cloud_provider=' + body.params.cloud_provider)
+                .auth(config.user, config.password)
+                .set('Accept', 'application/json')
+                .end(function(err, res) {
+                  if (err) throw err;
+                  console.log('delete storage resource {' + body.params.name + '} finished!');
+                  return 'delete storage resource {' + body.params.name + '} finished!'
+                });
+              break
+            default:
+              console.error('operation {' + body.operation + '} not supported!');
+          };
+          break;
+        case 'NetworkResource':
+          switch (body.operation) {
+            case 'create':
+              request.post('/v1alpha/network_resources')
+                .auth(config.user, config.password)
+                .send(body.params)
+                .set('Content-Type', 'application/json')
+                .set('Accept', 'application/json')
+                .end(function(err, res) {
+                  if (err) throw err;
+                  console.log(res.body);
+                  return res.body;
+                });
+              break
+            case "delete":
+              request.post('/v1alpha/network_resources/' + body.params.name +
+                  '?cloud_provider=' + body.params.cloud_provider)
+                .auth(config.user, config.password)
+                .set('Accept', 'application/json')
+                .end(function(err, res) {
+                  if (err) throw err;
+                  console.log('delete network resource {' + body.params.name + '} finished!');
+                  return 'delete network resource {' + body.params.name + '} finished!'
+                });
+              break
+            default:
+              console.error('operation {' + body.operation + '} not supported!');
+          };
+          break;
+        default:
+          console.error('resource {' + body.kind + '} not supported!');
+      };
+      return null
+    })
+    .catch(console.error);
+
+  return value;
+};
--- osc-runtime-js/webpack.config.js
+++ osc-runtime-js/webpack.config.js
@@ -4,10 +4,20 @@ module.exports = {
   entry: "./src/osc_runtime_bundler.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/",
     filename: "index.js",
+    libraryTarget: 'var',
+    library: 'EntryPoint'
   },
   mode: "development",
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    proxy: {
+      '/v1alpha': 'http://localhost:6106'
+    },
+    index: 'index.html',
+    compress: true
+  },
   node: {
     fs: "empty"
   }
